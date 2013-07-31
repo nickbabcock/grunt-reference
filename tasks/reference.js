34295bd0398617d3d6b6e83f9e696931b69110c5
Nick Babcock <nbabcock19@hotmail.com>
2013-07-31 04:16:37 -0500

    Indent with spaces

--- tasks/reference.js
+++ tasks/reference.js
@@ -1,89 +1,89 @@
 module.exports = function(grunt) {
-	'use strict';
+    'use strict';
 
-	var jsdom = require('jsdom');
-	var path = require('path');
-	var fs = require('fs');
-	var request = require('request');
+    var jsdom = require('jsdom');
+    var path = require('path');
+    var fs = require('fs');
+    var request = require('request');
     var async = require('async');
 
-	var scripts = [
-		'//ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js',
-		'//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.1/underscore-min.js'
-	];
+    var scripts = [
+        '//ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js',
+        '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.1/underscore-min.js'
+    ];
 
-	var documentToSource = function(document) {
-		return document.doctype.toString() + document.innerHTML;
-	};
+    var documentToSource = function(document) {
+        return document.doctype.toString() + document.innerHTML;
+    };
 
-	grunt.registerMultiTask('renderReferencePage', function() {
-		var done = this.async();
+    grunt.registerMultiTask('renderReferencePage', function() {
+        var done = this.async();
 
-		var filePath = '/mnt/Windows/nbsoftsolutions/_site/blog/reference-track-sheet.html';
-		jsdom.env(filePath, scripts, function(errors, window) {
+        var filePath = '/mnt/Windows/nbsoftsolutions/_site/blog/reference-track-sheet.html';
+        jsdom.env(filePath, scripts, function(errors, window) {
 
-			var $ = window.$;
-			var _ = window._;
+            var $ = window.$;
+            var _ = window._;
 
-			var contents = fs.readFileSync(path.resolve('./__citeBuffer.js'));
-			contents = JSON.parse(contents);
+            var contents = fs.readFileSync(path.resolve('./__citeBuffer.js'));
+            contents = JSON.parse(contents);
 
-			contents = _.chain(contents)
-			.pluck('data')
-			.flatten()
-			.filter(function(val) { return !val.periodical && !val.website; })
-			.groupBy(function(val) { return val.requestResult.id; })
-			.map(function(val) {
-				return {
-					count: val.length,
-					title: val[0].requestResult.volumeInfo.title,
-					authors: val[0].requestResult.volumeInfo.authors,
-					description: val[0].requestResult.volumeInfo.description,
-					publishedDate: val[0].requestResult.volumeInfo.publishedDate,
-					publisher: val[0].requestResult.volumeInfo.publisher,
-					image: val[0].requestResult.volumeInfo.imageLinks.thumbnail
-				};
-			})
-			.sortBy(function(val) { return val.count; })
-			.value()
-			.reverse();
+            contents = _.chain(contents)
+            .pluck('data')
+            .flatten()
+            .filter(function(val) { return !val.periodical && !val.website; })
+            .groupBy(function(val) { return val.requestResult.id; })
+            .map(function(val) {
+                return {
+                    count: val.length,
+                    title: val[0].requestResult.volumeInfo.title,
+                    authors: val[0].requestResult.volumeInfo.authors,
+                    description: val[0].requestResult.volumeInfo.description,
+                    publishedDate: val[0].requestResult.volumeInfo.publishedDate,
+                    publisher: val[0].requestResult.volumeInfo.publisher,
+                    image: val[0].requestResult.volumeInfo.imageLinks.thumbnail
+                };
+            })
+            .sortBy(function(val) { return val.count; })
+            .value()
+            .reverse();
 
-			var templ = _.template($('#referencePageTmpl').html());
+            var templ = _.template($('#referencePageTmpl').html());
 
-			$('div.Product').append($('<div>').html(templ({ references: contents })));
-			$('script.jsdom').remove();
+            $('div.Product').append($('<div>').html(templ({ references: contents })));
+            $('script.jsdom').remove();
 
-			fs.writeFileSync(filePath, documentToSource(window.document));
-			done();
-		});
-	});
+            fs.writeFileSync(filePath, documentToSource(window.document));
+            done();
+        });
+    });
 
-	grunt.registerMultiTask('reference', function() {
-		var task = this;
-		var done = this.async();
+    grunt.registerMultiTask('reference', function() {
+        var task = this;
+        var done = this.async();
 
-		var files = this.filesSrc;
+        var files = this.filesSrc;
 
 
-		var processDOM = function(window, options, callback) {
-			var filePath = window.location.pathname;
-			var $ = window.$;
-			var _ = window._;
+        var processDOM = function(window, options, callback) {
+            var filePath = window.location.pathname;
+            var $ = window.$;
+            var _ = window._;
 
-			var referenceElements = $('[cite], cite');
+            var referenceElements = $('[cite], cite');
 
-			var citationsCompleted = 0;
-			var requests = _.map(referenceElements, function(val, ind, arr) {
+            var citationsCompleted = 0;
+            var requests = _.map(referenceElements, function(val, ind, arr) {
                 var citation = $.trim(val.getAttribute('cite') || $(val).text());
 
-				var split = citation.split(' ');
-				var lookup = split[0];
-				var pageReference = split[1];
+                var split = citation.split(' ');
+                var lookup = split[0];
+                var pageReference = split[1];
 
                 return {
-					element: val,
-					elementIndex: ind,
-					page: parseInt(pageReference, 10) || 0,
+                    element: val,
+                    elementIndex: ind,
+                    page: parseInt(pageReference, 10) || 0,
                     periodical: lookup.indexOf('/') !== -1 && lookup.indexOf('http') === -1,
                     website: lookup.indexOf('http') !== -1,
                     lookup: lookup
@@ -98,104 +98,104 @@ module.exports = function(grunt) {
                 }
                 else if (newCitation.periodical) {
                     var urlP = 'http://api.altmetric.com/v1/doi/' + lookup;
-					request.get(urlP, function(error, response, body) {
-						body = JSON.parse(body);
-						newCitation.requestResult = body;
-						newCitation.requestResult.publishedDate = new Date(body.published_on * 1000);
-						newCitation.requestResult.id = newCitation.requestResult.doi;
-						newCitation.requestResult.authors = [];
-						lookupComplete();
-					});
+                    request.get(urlP, function(error, response, body) {
+                        body = JSON.parse(body);
+                        newCitation.requestResult = body;
+                        newCitation.requestResult.publishedDate = new Date(body.published_on * 1000);
+                        newCitation.requestResult.id = newCitation.requestResult.doi;
+                        newCitation.requestResult.authors = [];
+                        lookupComplete();
+                    });
                 }
                 else {
                     var urlG = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + lookup;
-					request.get(urlG, function(error, response, body) {
-						body = JSON.parse(body);
-						newCitation.requestResult = body.items[0];
-						newCitation.requestResult.volumeInfo.publishedDate = new Date(Date.parse(newCitation.requestResult.volumeInfo.publishedDate));
-						lookupComplete();
-					});
+                    request.get(urlG, function(error, response, body) {
+                        body = JSON.parse(body);
+                        newCitation.requestResult = body.items[0];
+                        newCitation.requestResult.volumeInfo.publishedDate = new Date(Date.parse(newCitation.requestResult.volumeInfo.publishedDate));
+                        lookupComplete();
+                    });
                 }
             }, function(err) {
-			    callback(window, requests, options);
+                callback(window, requests, options);
             });
 
-			if (referenceElements.length === 0) {
-				callback(window, undefined);
-			}
-		};
+            if (referenceElements.length === 0) {
+                callback(window, undefined);
+            }
+        };
 
-		var mapIbids = function(arr) {
-			var mapOnId = function(element) {
-				return element.requestResult.id;
-			};
+        var mapIbids = function(arr) {
+            var mapOnId = function(element) {
+                return element.requestResult.id;
+            };
 
-			for (var i = arr.length - 1; i >= 0; i--) {
-				var next = arr.map(mapOnId)
-					.lastIndexOf(arr[i].requestResult.id, i - 1);
+            for (var i = arr.length - 1; i >= 0; i--) {
+                var next = arr.map(mapOnId)
+                    .lastIndexOf(arr[i].requestResult.id, i - 1);
 
-				if (next <= 0 && arr[0].requestResult.id !== arr[i].requestResult.id) {
-					arr[i].ibidDifference = -1;
-				}
-				else {
-					arr[i].ibidDifference = i - next;
-				}
-			}
-		};
+                if (next <= 0 && arr[0].requestResult.id !== arr[i].requestResult.id) {
+                    arr[i].ibidDifference = -1;
+                }
+                else {
+                    arr[i].ibidDifference = i - next;
+                }
+            }
+        };
 
-		var domRequestsCompleted = function(window, data, options) {
-			var $ = window.$;
-			var _ = window._;
+        var domRequestsCompleted = function(window, data, options) {
+            var $ = window.$;
+            var _ = window._;
 
-			mapIbids(data);
+            mapIbids(data);
 
-			var container = $('#' + options.referenceContainer);
-			var elementTemplate = _.template($('#' + options.elementTemplateId).html());
-			var containerTemplate = _.template($('#' + options.referenceTemplateId).html());
+            var container = $('#' + options.referenceContainer);
+            var elementTemplate = _.template($('#' + options.elementTemplateId).html());
+            var containerTemplate = _.template($('#' + options.referenceTemplateId).html());
 
-			data.forEach(function(val, ind) {
-				$(val.element).after(elementTemplate({ i: ind }));
-			});
+            data.forEach(function(val, ind) {
+                $(val.element).after(elementTemplate({ i: ind }));
+            });
 
-			container.html(containerTemplate({ references: data }));
+            container.html(containerTemplate({ references: data }));
 
-			$('script.jsdom').remove();
+            $('script.jsdom').remove();
 
-			fs.writeFileSync(window.location.pathname, documentToSource(window.document));
-		};
+            fs.writeFileSync(window.location.pathname, documentToSource(window.document));
+        };
 
-		var fileJSON = [];
+        var fileJSON = [];
         async.each(files, function(f, callback) {
-			jsdom.env(path.resolve(f), scripts, function(errors, window) {
-				if (!errors){
-					processDOM(window, task.options(), function(window, data, options) {
-						if (data !== undefined) {
-							domRequestsCompleted(window, data, options);
+            jsdom.env(path.resolve(f), scripts, function(errors, window) {
+                if (!errors){
+                    processDOM(window, task.options(), function(window, data, options) {
+                        if (data !== undefined) {
+                            domRequestsCompleted(window, data, options);
 
-							data.forEach(function(val, ind) {
-								delete val.element;
-								delete val.elementIndex;
-							});
+                            data.forEach(function(val, ind) {
+                                delete val.element;
+                                delete val.elementIndex;
+                            });
 
-							var newFile = {
-								title: window.document.title,
-								path: window.location.pathname,
-								data: data
-							};
+                            var newFile = {
+                                title: window.document.title,
+                                path: window.location.pathname,
+                                data: data
+                            };
 
-							fileJSON.push(newFile);
-						}
+                            fileJSON.push(newFile);
+                        }
                         callback();
-					});
-				}			
-				else {
-					grunt.log.writeln('error');
+                    });
+                }           
+                else {
+                    grunt.log.writeln('error');
                     callback();
-				}	
-			});
+                }   
+            });
         }, function(error) {
-			fs.writeFileSync('./__citeBuffer.js', JSON.stringify(fileJSON, null, '\t'));
+            fs.writeFileSync('./__citeBuffer.js', JSON.stringify(fileJSON, null, '\t'));
             done();
         });
-	});
+    });
 };
